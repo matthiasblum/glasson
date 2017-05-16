b38e3edef2d61fe67adfec860fd7ef5f094e78ae
Matthias Blum <mat.blum@gmail.com>
2017-05-16 21:45:44 +0100

    Writing in C, reading in Py

--- glacon.c
+++ glacon.c
@@ -13,14 +13,40 @@
 
 uint32_t read_uint32(uint8_t *buffer) {
     // LE
-    return (buffer[0]<<0) | (buffer[1]<<8) | (buffer[2]<<16) | (buffer[3]<<24);
+    return buffer[0] | (buffer[1]<<8) | (buffer[2]<<16) | (buffer[3]<<24);
 
     // BE
-    //return (buffer[3]<<0) | (buffer[2]<<8) | (buffer[1]<<16) | (buffer[0]<<24);
+    //return buffer[3] | (buffer[2]<<8) | (buffer[1]<<16) | (buffer[0]<<24);
+}
 
+void write_uint32(FILE *fp, uint32_t i) {
+    uint8_t buffer[4];
+
+    // BE
+    // buffer[0] = (i >> 24) & 0xFF;
+    // buffer[1] = (i >> 16) & 0xFF;
+    // buffer[2] = (i >> 8) & 0xFF;
+    // buffer[3] = i & 0xFF;
+
+    // LE
+    buffer[0] = i & 0xFF;
+    buffer[1] = (i >> 8) & 0xFF;
+    buffer[2] = (i >> 16) & 0xFF;
+    buffer[3] = (i >> 24) & 0xFF;
+
+    fwrite(&buffer, sizeof(uint8_t), 4, fp);
 }
 
 int main(int argc, char const *argv[]) {
+    FILE *fp = fopen("test.dat", "w");
+    uint32_t i = 1989;
+
+    write_uint32(fp, i);
+
+
+
+    fclose(fp);
+
     /*FILE *fp;
     fp = fopen("test.dat", "r");
 
@@ -39,17 +65,17 @@ int main(int argc, char const *argv[]) {
     fclose(fp);
     return 0;*/
 
-    FILE *fp = fopen("test.dat", "w");
-
-    z_stream strm;
-    int ret, flush;
-    /* allocate deflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    ret = deflateInit(&strm, level);
-    if (ret != Z_OK)
-        return ret;
+    // FILE *fp = fopen("test.dat", "w");
+    //
+    // z_stream strm;
+    // int ret, flush;
+    // /* allocate deflate state */
+    // strm.zalloc = Z_NULL;
+    // strm.zfree = Z_NULL;
+    // strm.opaque = Z_NULL;
+    // ret = deflateInit(&strm, level);
+    // if (ret != Z_OK)
+    //     return ret;
 
 
 
@@ -58,15 +84,15 @@ int main(int argc, char const *argv[]) {
     // printf("%s\n", buffer);
     //
     // z_stream defstream;
-    z_stream infstream;
-    infstream.zalloc = Z_NULL;
-    infstream.zfree = Z_NULL;
-    infstream.opaque = Z_NULL;
-    // // setup "b" as the input and "c" as the compressed output
-    infstream.avail_in = (uInt)((char*)defstream.next_out - buffer); // size of input
-    infstream.next_in = (Bytef *)buffer; // input char array
-    infstream.avail_out = (uInt)sizeof(unc_buffer); // size of output
-    infstream.next_out = (Bytef *)unc_buffer; // output char array
+    // z_stream infstream;
+    // infstream.zalloc = Z_NULL;
+    // infstream.zfree = Z_NULL;
+    // infstream.opaque = Z_NULL;
+    // setup "b" as the input and "c" as the compressed output
+    // infstream.avail_in = (uInt)((char*)defstream.next_out - buffer); // size of input
+    // infstream.next_in = (Bytef *)buffer; // input char array
+    // infstream.avail_out = (uInt)sizeof(unc_buffer); // size of output
+    // infstream.next_out = (Bytef *)unc_buffer; // output char array
     //
     // // the actual DE-compression work.
     // inflateInit(&infstream);
