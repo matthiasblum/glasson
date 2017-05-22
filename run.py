6abb614e7a339e043773f0b3960c0562107696d4
Matthias Blum <mat.blum@gmail.com>
2017-05-22 17:16:36 +0100

    No aggregation option

--- run.py
+++ run.py
@@ -10,24 +10,26 @@ import glacon
 
 
 def main():
-    # url = 'http://hglab.org/data/SRX2179260.gla'
-    # filename = '/home/mblum/Dropbox/Perso/labs/igbmc_2013_strasbourg/HiC/Ren/SRX2179260/SRX2179260.gla'
-    # if not os.path.isfile(filename):
-    #     filename = '/home/matthias/Dropbox/Perso/labs/igbmc_2013_strasbourg/HiC/Ren/SRX2179260/SRX2179260.gla'
+    # filename = '/home/mblum/work/BingRen/AdrenalGland.gla'
     #
-    # fh = glasson.open(filename)
-    #
-    # # chr1:0-chrM:16,571 & chr4:7,862,541-chr15:94,379,315 [offset 0,4306017:0,0]
-    # result = fh.query(('chr1', 0, 'chrM', 0), ('chr4', 7000000, 'chr15', 94000000), resolution=500000)
-    # #result = fh.query(('chr1', 0, 3100000), resolution=1000000)
-    #
-    # for chrom1, chrom2, rows, cols, values in result:
-    #     for i, j, v in zip(rows, cols, values):
-    #         print(chrom1, chrom2, i, j, v)
+    # with glacon.Glacon(filename, 'r') as fh:
+    #     result = fh.query(('chrun_gl000217', 0, 3100000), resolution=1000000)
+    #     for chrom1, chrom2, rows, cols, values in result:
+    #         for i, j, v in zip(rows, cols, values):
+    #             print(chrom1, chrom2, i, j, v)
     #
     #
     #
-    # fh.close()
+    #     # # chr1:0-chrM:16,571 & chr4:7,862,541-chr15:94,379,315 [offset 0,4306017:0,0]
+    #     # result = fh.query(('chr1', 0, 'chrM', 0), ('chr4', 7000000, 'chr15', 94000000), resolution=500000)
+    #     # #result = fh.query(('chr1', 0, 3100000), resolution=1000000)
+    #     #
+    #     # for chrom1, chrom2, rows, cols, values in result:
+    #     #     for i, j, v in zip(rows, cols, values):
+    #     #         print(chrom1, chrom2, i, j, v)
+    #
+    #
+    #
     # return
 
 
@@ -36,18 +38,19 @@ def main():
     subparsers.required = True
 
     parser_create = subparsers.add_parser('store', help='Store Hi-C contact maps')
-    parser_create.add_argument('chrom_sizes', metavar='chrom.sizes', help='UCSC chromosome sizes file')
+    parser_create.add_argument('assembly', metavar='assembly', help='UCSC assembly (e.g. hg19) or chrom sizes file')
     parser_create.add_argument('output', help='output file')
     parser_create.add_argument('-f', dest='bed_files', help='fragment BED files', nargs='+', required=True)
     parser_create.add_argument('-m', dest='mat_files', help='matrix files', nargs='+', required=True)
 
-    parser_create.add_argument('--buffersize', type=int, metavar='INT', default=0, help='buffer size')
-    parser_create.add_argument('-p', dest='processes', type=int, metavar='INT', default=1, help='process threads')
-    parser_create.add_argument('-q', dest='quiet', action='store_true', default=False,
-                               help='do not print progress messages')
+    parser_create.add_argument('--buffersize', type=int, metavar='INT', default=0, help='buffer size (default: 0)')
+    parser_create.add_argument('-p', dest='processes', type=int, metavar='INT', default=1, help='process threads (default: 1)')
+    parser_create.add_argument('-q', '--quiet', action='store_true', default=False,
+                               help='do not print progress messages (default: off)')
     parser_create.add_argument('--tmp', default=tempfile.gettempdir(),
                                help='temporary directory (default: {})'.format(tempfile.gettempdir()))
-
+    parser_create.add_argument('--no-agg', dest='no_agg', action='store_true', default=False,
+                               help='do not aggregate maps (default: off)')
 
     args = parser.parse_args()
 
@@ -62,10 +65,11 @@ def main():
                              'and matrix files ({})\n'.format(len(args.bed_files), len(args.mat_files)))
             exit(1)
 
-        glacon.create(args.chrom_sizes, args.bed_files, args.mat_files, args.output,
+        glacon.create(args.assembly, args.bed_files, args.mat_files, args.output,
                       buffersize=args.buffersize,
                       processes=args.processes,
                       verbose=(not args.quiet),
+                      aggregate=(not args.no_agg),
                       tmp=args.tmp)
 
 
